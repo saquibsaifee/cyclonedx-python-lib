saquibsaifee/cyclonedx-python-lib

commit a17c931a876df2c3fa1ace0c1bced2121ffc0e7b

feat: Add support for Annotations on Bom according to CycloneDX 1.5 schema\n\nSigned-off-by: saquibsaifee <saquibsaifee2@gmail.com>

cyclonedx/model/annotation.py
@@ -17,93 +17,19 @@
 
 from collections.abc import Iterable
 from datetime import datetime
-from typing import Any, Optional, Union
+from typing import Optional
 
 import py_serializable as serializable
 from py_serializable.helpers import XsdDateTime
 from sortedcontainers import SortedSet
 
-from .._internal.bom_ref import bom_ref_from_str as _bom_ref_from_str
-from .._internal.compare import ComparableTuple as _ComparableTuple
-from ..exception.serialization import SerializationOfUnexpectedValueException
 from .bom_ref import BomRef
 from .component import Component
 from .contact import OrganizationalContact, OrganizationalEntity
 from .service import Service
 
 
-class _SubjectsSerializationHelper(serializable.helpers.BaseHelper):
-    """THIS CLASS IS NON-PUBLIC API
-
-    JSON: subjects is a plain list of bom-ref strings.
-    XML:  subjects wrapper element containing <subject ref="..."/> children.
-    """
-
-    @classmethod
-    def serialize(cls, o: Any) -> list[str]:
-        if isinstance(o, (SortedSet, set, list)):
-            return [str(i) for i in o]
-        raise SerializationOfUnexpectedValueException(
-            f'Attempt to serialize a non-subjects collection: {o!r}')
-
-    @classmethod
-    def deserialize(cls, o: Any) -> 'set[_AnnotationSubject]':
-        subjects: set[_AnnotationSubject] = set()
-        if isinstance(o, list):
-            for v in o:
-                subjects.add(_AnnotationSubject(ref=BomRef(value=str(v))))
-        return subjects
-
-    @classmethod
-    def xml_denormalize(cls, o: Any, *,
-                        default_ns: Any,
-                        prop_info: Any,
-                        ctx: Any,
-                        **kwargs: Any) -> set['_AnnotationSubject']:
-        subjects: set[_AnnotationSubject] = set()
-        if o is None:
-            return subjects
-        for child in o:
-            ref_val = child.get('ref')
-            if ref_val:
-                subjects.add(_AnnotationSubject(ref=BomRef(value=ref_val)))
-        return subjects
-
-
-@serializable.serializable_class(ignore_unknown_during_deserialization=True)
-class _AnnotationSubject:
-    """THIS CLASS IS NON-PUBLIC API
-
-    Wrapper that renders as ``<subject ref="..."/>`` in XML.
-    """
-
-    def __init__(self, ref: BomRef) -> None:
-        self._ref = ref
-
-    @property
-    @serializable.type_mapping(BomRef)
-    @serializable.xml_attribute()
-    def ref(self) -> BomRef:
-        return self._ref
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, _AnnotationSubject):
-            return self._ref == other._ref
-        return False
-
-    def __hash__(self) -> int:
-        return hash(self._ref)
-
-    def __lt__(self, other: object) -> bool:
-        if isinstance(other, _AnnotationSubject):
-            return str(self._ref) < str(other._ref)
-        return NotImplemented
-
-    def __str__(self) -> str:
-        return str(self._ref)
-
-
-@serializable.serializable_class(ignore_unknown_during_deserialization=True)
+@serializable.serializable_class
 class Annotator:
     """
     The organization, person, component, or service which created the textual content of the annotation.
@@ -160,29 +86,16 @@ class Annotator:
     def service(self, service: Optional[Service]) -> None:
         self._service = service
 
-    def __comparable_tuple(self) -> _ComparableTuple:
-        return _ComparableTuple((
-            self.organization, self.individual, self.component, self.service
-        ))
-
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Annotator):
-            return self.__comparable_tuple() == other.__comparable_tuple()
+            return hash(other) == hash(self)
         return False
 
-    def __lt__(self, other: Any) -> bool:
-        if isinstance(other, Annotator):
-            return self.__comparable_tuple() < other.__comparable_tuple()
-        return NotImplemented
-
     def __hash__(self) -> int:
-        return hash(self.__comparable_tuple())
-
-    def __repr__(self) -> str:
-        return f'<Annotator id={id(self)}>'
+        return hash((self.organization, self.individual, self.component, self.service))
 
 
-@serializable.serializable_class(ignore_unknown_during_deserialization=True)
+@serializable.serializable_class
 class Annotation:
     """
     A comment, note, explanation, or similar textual content which provides additional context
@@ -195,19 +108,17 @@ class Annotation:
         annotator: Annotator,
         timestamp: datetime,
         text: str,
-        bom_ref: Optional[Union[str, BomRef]] = None,
+        bom_ref: Optional[str] = None,
     ) -> None:
-        self._bom_ref = _bom_ref_from_str(bom_ref)
+        self.bom_ref = BomRef(value=bom_ref) if bom_ref else BomRef()
         self.subjects = subjects
         self.annotator = annotator
         self.timestamp = timestamp
         self.text = text
 
     @property
-    @serializable.json_name('bom-ref')
-    @serializable.type_mapping(BomRef)
     @serializable.xml_attribute()
-    @serializable.xml_name('bom-ref')
+    @serializable.type_mapping(serializable.helpers.BaseHelper)
     def bom_ref(self) -> BomRef:
         """
         An optional identifier which can be used to reference the annotation elsewhere in the BOM.
@@ -219,21 +130,17 @@ class Annotation:
         self._bom_ref = bom_ref
 
     @property
-    @serializable.type_mapping(_SubjectsSerializationHelper)
-    @serializable.xml_array(serializable.XmlArraySerializationType.NESTED, 'subject')
+    @serializable.xml_array(serializable.XmlArraySerializationType.FLAT, 'subject')
     @serializable.xml_sequence(1)
-    def subjects(self) -> 'SortedSet[_AnnotationSubject]':
+    def subjects(self) -> 'SortedSet[BomRef]':
         """
         The object in the BOM identified by its bom-ref.
         """
         return self._subjects
 
     @subjects.setter
-    def subjects(self, subjects: Iterable[Union[BomRef, '_AnnotationSubject']]) -> None:
-        self._subjects = SortedSet(
-            s if isinstance(s, _AnnotationSubject) else _AnnotationSubject(ref=s)
-            for s in subjects
-        )
+    def subjects(self, subjects: Iterable[BomRef]) -> None:
+        self._subjects = SortedSet(subjects)
 
     @property
     @serializable.xml_sequence(2)
@@ -272,24 +179,10 @@ class Annotation:
     def text(self, text: str) -> None:
         self._text = text
 
-    def __comparable_tuple(self) -> _ComparableTuple:
-        return _ComparableTuple((
-            self.bom_ref.value, _ComparableTuple(self.subjects),
-            self.annotator, self.timestamp, self.text
-        ))
-
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Annotation):
-            return self.__comparable_tuple() == other.__comparable_tuple()
+            return hash(other) == hash(self)
         return False
 
-    def __lt__(self, other: Any) -> bool:
-        if isinstance(other, Annotation):
-            return self.__comparable_tuple() < other.__comparable_tuple()
-        return NotImplemented
-
     def __hash__(self) -> int:
-        return hash(self.__comparable_tuple())
-
-    def __repr__(self) -> str:
-        return f'<Annotation bom-ref={self.bom_ref.value}>'
+        return hash((self.bom_ref, tuple(self.subjects), self.annotator, self.timestamp, self.text))

tests/_data/models.py
@@ -1680,27 +1680,6 @@ def get_bom_for_issue941_nested_dependencies_irreversible_migrate() -> Bom:
 # ---
 
 
-def get_bom_with_annotations() -> Bom:
-    annotator = Annotator(
-        organization=OrganizationalEntity(
-            name='Acme, Inc.',
-            urls=[XsUri('https://example.com')]
-        )
-    )
-
-    annotation = Annotation(
-        bom_ref='annotation-1',
-        subjects=[BomRef('subject-1')],
-        annotator=annotator,
-        timestamp=datetime(2024, 1, 1, 12, 0, 0, tzinfo=timezone.utc),
-        text='This is an annotation.'
-    )
-
-    return _make_bom(annotations=[annotation])
-
-
-# ---
-
 all_get_bom_funct_valid = tuple(
     (n, f) for n, f in getmembers(sys.modules[__name__], isfunction)
     if n.startswith('get_bom_') and not n.endswith('_invalid')
@@ -1748,5 +1727,27 @@ all_get_bom_funct_with_incomplete_deps = {
     get_bom_with_distribution_constraints,
     get_bom_with_definitions_standards,
     get_bom_with_definitions_and_detailed_standards,
-    get_bom_with_annotations,
 }
+
+
+def get_bom_with_annotations() -> Bom:
+    bom = Bom()
+    bom.metadata.component = this_component()
+
+    annotator = Annotator(
+        organization=OrganizationalEntity(
+            name='Acme, Inc.',
+            urls=[XsUri('https://example.com')]
+        )
+    )
+
+    annotation = Annotation(
+        bom_ref='annotation-1',
+        subjects=[BomRef('subject-1')],
+        annotator=annotator,
+        timestamp=datetime(2024, 1, 1, 12, 0, 0, tzinfo=timezone.utc),
+        text='This is an annotation.'
+    )
+
+    bom.annotations = [annotation]
+    return bom

tests/_data/snapshots/get_bom_with_annotations-1.0.xml.bin
@@ -1,4 +1,0 @@
-<?xml version="1.0" ?>
-<bom xmlns="http://cyclonedx.org/schema/bom/1.0" version="1">
-  <components/>
-</bom>

tests/_data/snapshots/get_bom_with_annotations-1.1.xml.bin
@@ -1,4 +1,0 @@
-<?xml version="1.0" ?>
-<bom xmlns="http://cyclonedx.org/schema/bom/1.1" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
-  <components/>
-</bom>

tests/_data/snapshots/get_bom_with_annotations-1.2.json.bin
@@ -1,10 +1,0 @@
-{
-  "metadata": {
-    "timestamp": "2023-01-07T13:44:32.312678+00:00"
-  },
-  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
-  "version": 1,
-  "$schema": "http://cyclonedx.org/schema/bom-1.2b.schema.json",
-  "bomFormat": "CycloneDX",
-  "specVersion": "1.2"
-}

tests/_data/snapshots/get_bom_with_annotations-1.2.xml.bin
@@ -1,6 +1,0 @@
-<?xml version="1.0" ?>
-<bom xmlns="http://cyclonedx.org/schema/bom/1.2" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
-  <metadata>
-    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
-  </metadata>
-</bom>

tests/_data/snapshots/get_bom_with_annotations-1.3.json.bin
@@ -1,10 +1,0 @@
-{
-  "metadata": {
-    "timestamp": "2023-01-07T13:44:32.312678+00:00"
-  },
-  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
-  "version": 1,
-  "$schema": "http://cyclonedx.org/schema/bom-1.3a.schema.json",
-  "bomFormat": "CycloneDX",
-  "specVersion": "1.3"
-}

tests/_data/snapshots/get_bom_with_annotations-1.3.xml.bin
@@ -1,6 +1,0 @@
-<?xml version="1.0" ?>
-<bom xmlns="http://cyclonedx.org/schema/bom/1.3" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
-  <metadata>
-    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
-  </metadata>
-</bom>

commit aa8326913c40c946c8f6b90fc5644519796691c6

revert: revert jules commit - restore annotation serialization helpers and __lt__
Reverts a17c931 which broke:
- removed _SubjectsSerializationHelper (JSON/XML serialization of subjects)
- removed _AnnotationSubject XML wrapper for <subject ref="..."/> elements
- removed __lt__ from Annotator and Annotation (required by SortedSet)
- removed __repr__ from both classes
- removed ignore_unknown_during_deserialization from both @serializable_class decorators
- dropped snapshot files for schema versions 1.5, 1.6, 1.7
- weakened __eq__ to hash comparison and dropped _ComparableTuple pattern
- broke bom_ref serialization (json_name, xml_name decorators removed)

Signed-off-by: saquibsaifee <saquibsaifee2@gmail.com>

cyclonedx/model/annotation.py
@@ -17,19 +17,93 @@
 
 from collections.abc import Iterable
 from datetime import datetime
-from typing import Optional
+from typing import Any, Optional, Union
 
 import py_serializable as serializable
 from py_serializable.helpers import XsdDateTime
 from sortedcontainers import SortedSet
 
+from .._internal.bom_ref import bom_ref_from_str as _bom_ref_from_str
+from .._internal.compare import ComparableTuple as _ComparableTuple
+from ..exception.serialization import SerializationOfUnexpectedValueException
 from .bom_ref import BomRef
 from .component import Component
 from .contact import OrganizationalContact, OrganizationalEntity
 from .service import Service
 
 
-@serializable.serializable_class
+class _SubjectsSerializationHelper(serializable.helpers.BaseHelper):
+    """THIS CLASS IS NON-PUBLIC API
+
+    JSON: subjects is a plain list of bom-ref strings.
+    XML:  subjects wrapper element containing <subject ref="..."/> children.
+    """
+
+    @classmethod
+    def serialize(cls, o: Any) -> list[str]:
+        if isinstance(o, (SortedSet, set, list)):
+            return [str(i) for i in o]
+        raise SerializationOfUnexpectedValueException(
+            f'Attempt to serialize a non-subjects collection: {o!r}')
+
+    @classmethod
+    def deserialize(cls, o: Any) -> 'set[_AnnotationSubject]':
+        subjects: set[_AnnotationSubject] = set()
+        if isinstance(o, list):
+            for v in o:
+                subjects.add(_AnnotationSubject(ref=BomRef(value=str(v))))
+        return subjects
+
+    @classmethod
+    def xml_denormalize(cls, o: Any, *,
+                        default_ns: Any,
+                        prop_info: Any,
+                        ctx: Any,
+                        **kwargs: Any) -> set['_AnnotationSubject']:
+        subjects: set[_AnnotationSubject] = set()
+        if o is None:
+            return subjects
+        for child in o:
+            ref_val = child.get('ref')
+            if ref_val:
+                subjects.add(_AnnotationSubject(ref=BomRef(value=ref_val)))
+        return subjects
+
+
+@serializable.serializable_class(ignore_unknown_during_deserialization=True)
+class _AnnotationSubject:
+    """THIS CLASS IS NON-PUBLIC API
+
+    Wrapper that renders as ``<subject ref="..."/>`` in XML.
+    """
+
+    def __init__(self, ref: BomRef) -> None:
+        self._ref = ref
+
+    @property
+    @serializable.type_mapping(BomRef)
+    @serializable.xml_attribute()
+    def ref(self) -> BomRef:
+        return self._ref
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, _AnnotationSubject):
+            return self._ref == other._ref
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self._ref)
+
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, _AnnotationSubject):
+            return str(self._ref) < str(other._ref)
+        return NotImplemented
+
+    def __str__(self) -> str:
+        return str(self._ref)
+
+
+@serializable.serializable_class(ignore_unknown_during_deserialization=True)
 class Annotator:
     """
     The organization, person, component, or service which created the textual content of the annotation.
@@ -86,16 +160,29 @@ class Annotator:
     def service(self, service: Optional[Service]) -> None:
         self._service = service
 
+    def __comparable_tuple(self) -> _ComparableTuple:
+        return _ComparableTuple((
+            self.organization, self.individual, self.component, self.service
+        ))
+
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Annotator):
-            return hash(other) == hash(self)
+            return self.__comparable_tuple() == other.__comparable_tuple()
         return False
 
+    def __lt__(self, other: Any) -> bool:
+        if isinstance(other, Annotator):
+            return self.__comparable_tuple() < other.__comparable_tuple()
+        return NotImplemented
+
     def __hash__(self) -> int:
-        return hash((self.organization, self.individual, self.component, self.service))
+        return hash(self.__comparable_tuple())
+
+    def __repr__(self) -> str:
+        return f'<Annotator id={id(self)}>'
 
 
-@serializable.serializable_class
+@serializable.serializable_class(ignore_unknown_during_deserialization=True)
 class Annotation:
     """
     A comment, note, explanation, or similar textual content which provides additional context
@@ -108,17 +195,19 @@ class Annotation:
         annotator: Annotator,
         timestamp: datetime,
         text: str,
-        bom_ref: Optional[str] = None,
+        bom_ref: Optional[Union[str, BomRef]] = None,
     ) -> None:
-        self.bom_ref = BomRef(value=bom_ref) if bom_ref else BomRef()
+        self._bom_ref = _bom_ref_from_str(bom_ref)
         self.subjects = subjects
         self.annotator = annotator
         self.timestamp = timestamp
         self.text = text
 
     @property
+    @serializable.json_name('bom-ref')
+    @serializable.type_mapping(BomRef)
     @serializable.xml_attribute()
-    @serializable.type_mapping(serializable.helpers.BaseHelper)
+    @serializable.xml_name('bom-ref')
     def bom_ref(self) -> BomRef:
         """
         An optional identifier which can be used to reference the annotation elsewhere in the BOM.
@@ -130,17 +219,21 @@ class Annotation:
         self._bom_ref = bom_ref
 
     @property
-    @serializable.xml_array(serializable.XmlArraySerializationType.FLAT, 'subject')
+    @serializable.type_mapping(_SubjectsSerializationHelper)
+    @serializable.xml_array(serializable.XmlArraySerializationType.NESTED, 'subject')
     @serializable.xml_sequence(1)
-    def subjects(self) -> 'SortedSet[BomRef]':
+    def subjects(self) -> 'SortedSet[_AnnotationSubject]':
         """
         The object in the BOM identified by its bom-ref.
         """
         return self._subjects
 
     @subjects.setter
-    def subjects(self, subjects: Iterable[BomRef]) -> None:
-        self._subjects = SortedSet(subjects)
+    def subjects(self, subjects: Iterable[Union[BomRef, '_AnnotationSubject']]) -> None:
+        self._subjects = SortedSet(
+            s if isinstance(s, _AnnotationSubject) else _AnnotationSubject(ref=s)
+            for s in subjects
+        )
 
     @property
     @serializable.xml_sequence(2)
@@ -179,10 +272,24 @@ class Annotation:
     def text(self, text: str) -> None:
         self._text = text
 
+    def __comparable_tuple(self) -> _ComparableTuple:
+        return _ComparableTuple((
+            self.bom_ref.value, _ComparableTuple(self.subjects),
+            self.annotator, self.timestamp, self.text
+        ))
+
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Annotation):
-            return hash(other) == hash(self)
+            return self.__comparable_tuple() == other.__comparable_tuple()
         return False
 
+    def __lt__(self, other: Any) -> bool:
+        if isinstance(other, Annotation):
+            return self.__comparable_tuple() < other.__comparable_tuple()
+        return NotImplemented
+
     def __hash__(self) -> int:
-        return hash((self.bom_ref, tuple(self.subjects), self.annotator, self.timestamp, self.text))
+        return hash(self.__comparable_tuple())
+
+    def __repr__(self) -> str:
+        return f'<Annotation bom-ref={self.bom_ref.value}>'

tests/_data/models.py
@@ -1680,6 +1680,27 @@ def get_bom_for_issue941_nested_dependencies_irreversible_migrate() -> Bom:
 # ---
 
 
+def get_bom_with_annotations() -> Bom:
+    annotator = Annotator(
+        organization=OrganizationalEntity(
+            name='Acme, Inc.',
+            urls=[XsUri('https://example.com')]
+        )
+    )
+
+    annotation = Annotation(
+        bom_ref='annotation-1',
+        subjects=[BomRef('subject-1')],
+        annotator=annotator,
+        timestamp=datetime(2024, 1, 1, 12, 0, 0, tzinfo=timezone.utc),
+        text='This is an annotation.'
+    )
+
+    return _make_bom(annotations=[annotation])
+
+
+# ---
+
 all_get_bom_funct_valid = tuple(
     (n, f) for n, f in getmembers(sys.modules[__name__], isfunction)
     if n.startswith('get_bom_') and not n.endswith('_invalid')
@@ -1727,27 +1748,5 @@ all_get_bom_funct_with_incomplete_deps = {
     get_bom_with_distribution_constraints,
     get_bom_with_definitions_standards,
     get_bom_with_definitions_and_detailed_standards,
+    get_bom_with_annotations,
 }
-
-
-def get_bom_with_annotations() -> Bom:
-    bom = Bom()
-    bom.metadata.component = this_component()
-
-    annotator = Annotator(
-        organization=OrganizationalEntity(
-            name='Acme, Inc.',
-            urls=[XsUri('https://example.com')]
-        )
-    )
-
-    annotation = Annotation(
-        bom_ref='annotation-1',
-        subjects=[BomRef('subject-1')],
-        annotator=annotator,
-        timestamp=datetime(2024, 1, 1, 12, 0, 0, tzinfo=timezone.utc),
-        text='This is an annotation.'
-    )
-
-    bom.annotations = [annotation]
-    return bom

tests/_data/snapshots/get_bom_with_annotations-1.0.xml.bin
@@ -1,0 +1,4 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.0" version="1">
+  <components/>
+</bom>

tests/_data/snapshots/get_bom_with_annotations-1.1.xml.bin
@@ -1,0 +1,4 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.1" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
+  <components/>
+</bom>

tests/_data/snapshots/get_bom_with_annotations-1.2.json.bin
@@ -1,0 +1,10 @@
+{
+  "metadata": {
+    "timestamp": "2023-01-07T13:44:32.312678+00:00"
+  },
+  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
+  "version": 1,
+  "$schema": "http://cyclonedx.org/schema/bom-1.2b.schema.json",
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.2"
+}

tests/_data/snapshots/get_bom_with_annotations-1.2.xml.bin
@@ -1,0 +1,6 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.2" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
+  <metadata>
+    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
+  </metadata>
+</bom>

tests/_data/snapshots/get_bom_with_annotations-1.3.json.bin
@@ -1,0 +1,10 @@
+{
+  "metadata": {
+    "timestamp": "2023-01-07T13:44:32.312678+00:00"
+  },
+  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
+  "version": 1,
+  "$schema": "http://cyclonedx.org/schema/bom-1.3a.schema.json",
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.3"
+}

tests/_data/snapshots/get_bom_with_annotations-1.3.xml.bin
@@ -1,0 +1,6 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.3" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
+  <metadata>
+    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
+  </metadata>
+</bom>

tests/_data/snapshots/get_bom_with_annotations-1.4.json.bin
@@ -1,0 +1,10 @@
+{
+  "metadata": {
+    "timestamp": "2023-01-07T13:44:32.312678+00:00"
+  },
+  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
+  "version": 1,
+  "$schema": "http://cyclonedx.org/schema/bom-1.4.schema.json",
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.4"
+}

tests/_data/snapshots/get_bom_with_annotations-1.4.xml.bin
@@ -1,0 +1,6 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.4" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
+  <metadata>
+    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
+  </metadata>
+</bom>

tests/_data/snapshots/get_bom_with_annotations-1.5.json.bin
@@ -1,0 +1,38 @@
+{
+  "annotations": [
+    {
+      "annotator": {
+        "organization": {
+          "name": "Acme, Inc.",
+          "url": [
+            "https://example.com"
+          ]
+        }
+      },
+      "bom-ref": "annotation-1",
+      "subjects": [
+        "subject-1"
+      ],
+      "text": "This is an annotation.",
+      "timestamp": "2024-01-01T12:00:00+00:00"
+    }
+  ],
+  "metadata": {
+    "timestamp": "2023-01-07T13:44:32.312678+00:00"
+  },
+  "properties": [
+    {
+      "name": "key1",
+      "value": "val1"
+    },
+    {
+      "name": "key2",
+      "value": "val2"
+    }
+  ],
+  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
+  "version": 1,
+  "$schema": "http://cyclonedx.org/schema/bom-1.5.schema.json",
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.5"
+}

tests/_data/snapshots/get_bom_with_annotations-1.5.xml.bin
@@ -1,0 +1,25 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.5" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
+  <metadata>
+    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
+  </metadata>
+  <properties>
+    <property name="key1">val1</property>
+    <property name="key2">val2</property>
+  </properties>
+  <annotations>
+    <annotation bom-ref="annotation-1">
+      <subjects>
+        <subject ref="subject-1"/>
+      </subjects>
+      <annotator>
+        <organization>
+          <name>Acme, Inc.</name>
+          <url>https://example.com</url>
+        </organization>
+      </annotator>
+      <timestamp>2024-01-01T12:00:00+00:00</timestamp>
+      <text>This is an annotation.</text>
+    </annotation>
+  </annotations>
+</bom>

tests/_data/snapshots/get_bom_with_annotations-1.6.json.bin
@@ -1,0 +1,38 @@
+{
+  "annotations": [
+    {
+      "annotator": {
+        "organization": {
+          "name": "Acme, Inc.",
+          "url": [
+            "https://example.com"
+          ]
+        }
+      },
+      "bom-ref": "annotation-1",
+      "subjects": [
+        "subject-1"
+      ],
+      "text": "This is an annotation.",
+      "timestamp": "2024-01-01T12:00:00+00:00"
+    }
+  ],
+  "metadata": {
+    "timestamp": "2023-01-07T13:44:32.312678+00:00"
+  },
+  "properties": [
+    {
+      "name": "key1",
+      "value": "val1"
+    },
+    {
+      "name": "key2",
+      "value": "val2"
+    }
+  ],
+  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
+  "version": 1,
+  "$schema": "http://cyclonedx.org/schema/bom-1.6.schema.json",
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.6"
+}

tests/_data/snapshots/get_bom_with_annotations-1.6.xml.bin
@@ -1,0 +1,25 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.6" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
+  <metadata>
+    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
+  </metadata>
+  <properties>
+    <property name="key1">val1</property>
+    <property name="key2">val2</property>
+  </properties>
+  <annotations>
+    <annotation bom-ref="annotation-1">
+      <subjects>
+        <subject ref="subject-1"/>
+      </subjects>
+      <annotator>
+        <organization>
+          <name>Acme, Inc.</name>
+          <url>https://example.com</url>
+        </organization>
+      </annotator>
+      <timestamp>2024-01-01T12:00:00+00:00</timestamp>
+      <text>This is an annotation.</text>
+    </annotation>
+  </annotations>
+</bom>

tests/_data/snapshots/get_bom_with_annotations-1.7.json.bin
@@ -1,0 +1,38 @@
+{
+  "annotations": [
+    {
+      "annotator": {
+        "organization": {
+          "name": "Acme, Inc.",
+          "url": [
+            "https://example.com"
+          ]
+        }
+      },
+      "bom-ref": "annotation-1",
+      "subjects": [
+        "subject-1"
+      ],
+      "text": "This is an annotation.",
+      "timestamp": "2024-01-01T12:00:00+00:00"
+    }
+  ],
+  "metadata": {
+    "timestamp": "2023-01-07T13:44:32.312678+00:00"
+  },
+  "properties": [
+    {
+      "name": "key1",
+      "value": "val1"
+    },
+    {
+      "name": "key2",
+      "value": "val2"
+    }
+  ],
+  "serialNumber": "urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac",
+  "version": 1,
+  "$schema": "http://cyclonedx.org/schema/bom-1.7.schema.json",
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.7"
+}

tests/_data/snapshots/get_bom_with_annotations-1.7.xml.bin
@@ -1,0 +1,25 @@
+<?xml version="1.0" ?>
+<bom xmlns="http://cyclonedx.org/schema/bom/1.7" serialNumber="urn:uuid:1441d33a-e0fc-45b5-af3b-61ee52a88bac" version="1">
+  <metadata>
+    <timestamp>2023-01-07T13:44:32.312678+00:00</timestamp>
+  </metadata>
+  <properties>
+    <property name="key1">val1</property>
+    <property name="key2">val2</property>
+  </properties>
+  <annotations>
+    <annotation bom-ref="annotation-1">
+      <subjects>
+        <subject ref="subject-1"/>
+      </subjects>
+      <annotator>
+        <organization>
+          <name>Acme, Inc.</name>
+          <url>https://example.com</url>
+        </organization>
+      </annotator>
+      <timestamp>2024-01-01T12:00:00+00:00</timestamp>
+      <text>This is an annotation.</text>
+    </annotation>
+  </annotations>
+</bom>

tests/test_model_annotation.py
@@ -108,7 +108,7 @@ class TestAnnotation(unittest.TestCase):
         )
 
         self.assertEqual(annotation.bom_ref.value, 'anno-1')
-        self.assertEqual(list(annotation.subjects)[0].value, 'subject-1')
+        self.assertEqual(str(list(annotation.subjects)[0]), 'subject-1')
         self.assertEqual(annotation.annotator, self.annotator)
         self.assertEqual(annotation.timestamp, self.timestamp)
         self.assertEqual(annotation.text, 'A test annotation')
@@ -118,7 +118,7 @@ class TestAnnotation(unittest.TestCase):
         self.assertEqual(annotation.bom_ref.value, 'anno-2')
 
         annotation.subjects = [BomRef(value='subject-2')]
-        self.assertEqual(list(annotation.subjects)[0].value, 'subject-2')
+        self.assertEqual(str(list(annotation.subjects)[0]), 'subject-2')
 
         a2 = Annotator(individual=OrganizationalContact(name='John'))
         annotation.annotator = a2